50c5fd9bd131eeed0741832aee4725116c6ccb86
Сергей <parenek.serega@gmail.com>
2021-11-21 21:24:42 +0300

    Exercise "07 wall" done

--- 07_wall.py
+++ 07_wall.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
+sd.resolution = (1200, 600)
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+x1, y1 = 0, 0
+x2, y2 = 100, 50
+point1 = sd.get_point(x1, y1)
+point2 = sd.get_point(x2, y2)
+for i in range(0, 601, 50):
+    y1 = i
+    y2 = i + 50
+    for j in range(0, 1201, 50):
+        if i % 20 != 0 and j % 20 == 0:
+            x1 = j
+            x2 = j + 100
+            point1 = sd.get_point(x1, y1)
+            point2 = sd.get_point(x2, y2)
+            sd.rectangle(point1, point2, width=3)
+        elif i % 20 == 0 and j % 20 != 0:
+            x1 = j
+            x2 = j + 100
+            point1 = sd.get_point(x1, y1)
+            point2 = sd.get_point(x2, y2)
+            sd.rectangle(point1, point2, width=3)
 
-simple_draw.pause()
+sd.pause()
